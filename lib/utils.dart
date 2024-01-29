03ffeed4c9d411f3f2381657241a535014998ccb
Iyxan23 <nurihsanalghifari@gmail.com>
2024-01-29 11:10:15 +0700

    fix: remove trailing 00 on formatIDR output

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,4 +1,4 @@
 String formatIDR(int number) {
-  return number.toStringAsFixed(2).replaceAllMapped(
+  return number.toStringAsFixed(0).replaceAllMapped(
       RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},');
 }
